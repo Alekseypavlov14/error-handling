480d8c9178bdabb981e4ea8cb6645c024ac40e7b
Alekseypavlov14 <aleshapavlov9@gmail.com>
2024-09-09 09:52:56 +0300

    added exports for inner types

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,11 @@ export { wrapWithErrorHandler } from './utils/wrap-with-error-handler'
 export { executeWithErrorHandler } from './utils/execute-with-error-handler'
 
 export { defaultHandler, alwaysHandler } from './constants'
+
+export { combine } from './utils/combine'
+export { merge } from './utils/merge'
+
+export type { HandlerCallback } from './types/handler-callback'
+export type { ObjectKey } from './types/object-key'
+export type { Selector } from './types/selector'
+export type { Config } from './types/config'
